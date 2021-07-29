534b3c3118987ab4b6fa345ec06df650a487a6ef
Steph2830 <84094385+Steph2830@users.noreply.github.com>
2021-07-29 11:01:06 -0230

    Update Trivia.js

--- Trivia.js
+++ Trivia.js
@@ -1,7 +1,32 @@
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 
 	function showQuestions(questions, quizContainer){
-		// code will go here
+		question: "What is the name of the currency that is used in the Fallout games?",
+		answers: {
+			a: 'BottleCaps',
+			b: 'NCR Dollars',
+			c: 'Chems',
+            d: 'Ammo'
+		}
+		correctAnswer: 'a'
+
+		question: "What does S.P.E.C.I.A.L. stand for?"
+		answers: {
+			a: 'Strength, Perception, Endurance, Charisma, Intelligence, Agility, Luck'
+			b: 'Sarcasm, Power, Energy, Creativity, Intuition, Anger, Leverage'
+			c: 'It means nothing'
+			d: 'Stealth, Precision, Empathy, Courage, Imagination, Ambivalence, Learning'
+		}
+		correctAnswer: 'a'
+
+
+
+
+
+
+
+	}
+	
 	}
 
 	function showResults(questions, quizContainer, resultsContainer){
